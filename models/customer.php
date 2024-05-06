7bb3dd580c6d337e87f1c1ad98d96adaa81c3718
khaledkaram510 <khaleddaib510@gmail.com>
2024-05-06 19:08:40 +0300

    c

--- models/customer.php
+++ models/customer.php
@@ -1,10 +1,99 @@
 <?php
 
-class customer
+class customer extends user
 {
-    private $paymentInfo;
-    private $adresses;
-    private $CusotmerID;
+    private $payment;
+    private $id;
+    private $phone;
+    private $shapping_info;
+    
+
+    public function getPayment()
+    {
+        return $this->payment;
+    }
+    public function setPayment($payment)
+    {
+        $this->payment=$payment;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id=$id;
+    }
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    public function setPhone($phone)
+    {
+        $this->phone=$phone;
+    }
+    public function getShapping_info()
+    {
+        return $this->shapping_info;
+    }
+    public function setShapping_info($shapping_info)
+    {
+        $this->shapping_info=$shapping_info;
+    }
+
+    // public add_payment_method(){
+
+    // }
+
+    public function add_payment_method()
+    {
+    }
+
+    public function remove_payment_method()
+    {
+    }
+
+    public function search()
+    {
+    }
+
+    public function filter()
+    {
+    }
+
+    public function refund()
+    {
+    }
+
+    public function change_shipping_address()
+    {
+    }
+
+    public function view_orders_history()
+    {
+    }
+
+    public function chat_with_ai()
+    {
+    }
+
+    public function view_recommendation()
+    {
+    }
+
+    public function update_profile()
+    {
+    }
+
+    public function register()
+    {
+    }
+
+
+    // public function forget_password()
+    // {
+    // }
+
     
 
 
--- models/user.php
+++ models/user.php
@@ -3,13 +3,9 @@
 class user
 
 {
-    private $payment;
-    private $id;
-    private $phone;
-    private $shapping_info;
     private $username;
     private $password;
-    
+
     public function __construct($username,$password)
     {
         $this->username=$username;
@@ -31,89 +27,9 @@ class user
     {
         $this->password=$password;
     }
-    public function getPayment()
+    public function login()
     {
-        return $this->payment;
-    }
-    public function setPayment($payment)
-    {
-        $this->payment=$payment;
-    }
-    public function getId()
-    {
-        return $this->id;
-    }
-    public function setId($id)
-    {
-        $this->id=$id;
-    }
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-    public function setPhone($phone)
-    {
-        $this->phone=$phone;
-    }
-    public function getShapping_info()
-    {
-        return $this->shapping_info;
-    }
-    public function setShapping_info($shapping_info)
-    {
-        $this->shapping_info=$shapping_info;
-    }
-
-    // public add_payment_method(){
-
-    // }
-
-    public function add_payment_method()
-    {
-    }
-
-    public function remove_payment_method()
-    {
-    }
-
-    public function search()
-    {
-    }
-
-    public function filter()
-    {
-    }
-
-    public function refund()
-    {
-    }
-
-    public function change_shipping_address()
-    {
-    }
-
-    public function view_orders_history()
-    {
-    }
-
-    public function chat_with_ai()
-    {
-    }
-
-    public function view_recommendation()
-    {
-    }
-
-    public function update_profile()
-    {
-    }
-
-    public function register()
-    {
-    }
-
-    public function forget_password()
-    {
+        
     }
 
 
