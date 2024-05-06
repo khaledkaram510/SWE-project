dc07462685a3881fd31820b7a058881cc6ae8089
khaledkaram510 <khaleddaib510@gmail.com>
2024-05-06 18:50:29 +0300

    add user

--- models/user.php
+++ models/user.php
@@ -6,6 +6,27 @@ class user
     private $username;
     private $password;
     
+    public function __construct($username,$password)
+    {
+        $this->username=$username;
+        $this->password=$password;
+    }
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setUsername($username)
+    {
+        $this->username=$username;
+    }
+    public function setPassword($password)
+    {
+        $this->password=$password;
+    }
 
 }
 
