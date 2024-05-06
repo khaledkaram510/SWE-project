7ab160218d4301c9988ce5133bfc3bd5cbf9945e
khaledkaram510 <khaleddaib510@gmail.com>
2024-05-06 18:54:43 +0300

    edit user

--- models/user.php
+++ models/user.php
@@ -3,6 +3,10 @@
 class user
 
 {
+    private $payment;
+    private $id;
+    private $phone;
+    private $shapping_info;
     private $username;
     private $password;
     
@@ -27,6 +31,39 @@ class user
     {
         $this->password=$password;
     }
+    public function getPayment()
+    {
+        return $this->payment;
+    }
+    public function setPayment($payment)
+    {
+        $this->payment=$payment;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id=$id;
+    }
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    public function setPhone($phone)
+    {
+        $this->phone=$phone;
+    }
+    public function getShapping_info()
+    {
+        return $this->shapping_info;
+    }
+    public function setShapping_info($shapping_info)
+    {
+        $this->shapping_info=$shapping_info;
+    }
+    
 
 }
 
