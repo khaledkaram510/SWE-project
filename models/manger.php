3a8f01d1ef138d0dfceedd7bd8ab12e6fa6fad98
khaledkaram510 <khaleddaib510@gmail.com>
2024-05-07 18:57:33 +0300

    finish manger

--- models/manger.php
+++ models/manger.php
@@ -4,12 +4,53 @@ class manger{
   private $mangerName;
   private $mangerEmail;
   private $mangerPassword;
+  private $db;
 
+  public function __construct()
+  {
+    $this->db = new database();
+    $con = $this->db->openConnection();
+    if(!$con)
+    {
+      echo "seller Not Connected";
+    }
+  }
+  public function listItem($cat)
+  { 
+    $str="SELECT * from items where category='$cat'";
+    $result = $this->db->query($str);
+    if (!$result) {
+      return false;
+    }
+    if((mysqli_num_rows($result)) == 0){
+      return false;
+    }
+    return $result;
+  }
+  public function deleteItem($item_id)
+  {
+    $str="DELETE FROM items WHERE i_id='$item_id'";
+    $this->db->query($str);
+  }
+  public function addItem($name ,$description,$price,$image,$rate ,$ammount,$offer,$catagory_name)
+  {
+    $str="INSERT INTO items VALUES  ('$name','$description','$price' , '$image' , '$rate','$ammount','$offer','$catagory_name')";
+    $this->db->query($str);
+  }
+  public function updateItem($id,$row_name,$new_value)
+  {
+    $str="UPDATE `item` SET `$row_name`=$new_value WHERE i_id= '$id'";
+    $this->db->query($str);
+  }
+  public function makeDiscount($item_id,$discount) {
+    $str="UPDATE `item` SET `discount`=$discount WHERE i_id= '$item_id'";
+    $this->db->query($str);
+  }
   public function setmangerID($mangerID) {
     $this->mangerID = $mangerID;
   }
   public function getMangerID() {
-
+    return $this->mangerID;
   }
   public function setMangerName($mangerName) {
     $this->mangerName = $mangerName;
@@ -29,22 +70,6 @@ class manger{
   public function getMangerPassword() {
     return $this->mangerPassword;
   }
-  public function ListingItem() {
-
-  }
-
-  public function updateItemDescription() {
-
-  }
-
-  public function makeDiscount() {
-
-  }
-
-  public function deleteItem() {
-
-  }
-
   public function viewReports() {
 
   }
