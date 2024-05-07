6598a4a548978708fd8146f9b19a14a257456eca
tadros12 <tadros_20220122@fci.helwan.edu.eg>
2024-05-08 01:05:37 +0300

    Revert "Merge branch 'main' of https://github.com/khaledkaram510/SWE-project"
    This reverts commit a6012f14c545021d617e742d1641004c95c6c7c7, reversing
    changes made to baa58eb365901f5d950528762cf5fa8fe0032ccd.

--- views/admin/index.php
+++ views/admin/index.php
@@ -75,11 +75,10 @@ function create_cards($selle,$catagoty){
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="../css/styles.css" rel="stylesheet" />
-        <link href="../css/adminstyle.css" rel="stylesheet" />
+        <link href="../css/addstyle.css" rel="stylesheet" />
     </head>
     <body>
         <!-- Navigation-->
-        <!-- <div></div> -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#!">Start Bootstrap</a>
@@ -132,9 +131,7 @@ function create_cards($selle,$catagoty){
                 }
                 ?>
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <a class="a" href="add_items.php">
-                        <div class="add card">+</div>
-                    </a>
+                    <div class="add card">+</div>
                 </div>
             <!-- <h1>hello</h1> -->
             </div>
